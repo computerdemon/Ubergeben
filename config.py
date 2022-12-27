3ff85a44d058e46a69fec9d0e852d26cf9cdec93
Bryan <computer.demon@gmail.com>
2022-12-27 16:02:35 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+# DISCORD VARIABLES - INSERT YOUR INFORMATION
 TOKEN = 'your_token_here'
 CHANNEL = 'your_channel_here'
+
+# AZURE VARIABLES - INSERT YOUR INFORMATION
+speech_key = 'your_Azure_speech_key_here'
+region = "your_Azure_speech_region_here"
